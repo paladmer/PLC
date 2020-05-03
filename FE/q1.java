b72b5b5e23b6e3a279ee39c4c704eec5b70e8a22
mpalad <48260594+paladmer@users.noreply.github.com>
2020-05-02 21:06:46 -0400

    Update q1.java

--- FE/q1.java
+++ FE/q1.java
@@ -18,220 +18,188 @@ public class q1 {
 		  				   + "D) Variable Names \n" );
 		  
 		  Scanner console = new Scanner (System.in);
-		  System.out.println("Enter your selection letter:  ");
+		  
+
+int attempts = 10; 
+int begin; 
+for (begin = 1 ; begin <=attempts ; begin++) { // Start of begin FOR Loop
+
+
+		  System.out.println("**Enter your selection letter:**");
 		  String select = console.nextLine(); 
 		  
+/**************************************************************************************************************/
 		  /** A) Floating-Point Literals choice **/
-		  if (select.equalsIgnoreCase("A")) {
-			
+		  if (select.equalsIgnoreCase("A")) { 
 			  
+		
+	
+			  System.out.println("Enter a floating point literal: ");
+			  String float_lit = console.nextLine();
+			  Pattern pattern = Pattern.compile("[+-]?([0-9]*[.])?[0-9]+[fFlL]?"); // the Rule 
+		 
 			  
+		  	  Matcher check = pattern.matcher(float_lit); // What is being checked against the rule
+		  	  boolean result = check.matches(); // if the rule applies to it , then it returns true meaning valid input
+				  	
+		  	  	   if (result == true) {
+		  	  		   
+		  	  		   		
+		  	  		   		// prints out the valid numbers like 100f	
+		  	  		   		if (!float_lit.contains(".")) {
+		  	  		   			System.out.println("Valid float point Literal: "  + float_lit); 
+		  	  		   		}
+				  		
+		  	  		   		// Invalids such as 99.9
+					  		if (float_lit.contains(".")&!(float_lit.endsWith("f")|float_lit.endsWith("L")| float_lit.endsWith("l")|float_lit.endsWith("F"))){
+					  			System.out.println("Invalid float point Literal: "  +  float_lit);
+					  		
+					  		// Valid like 99.9f
+					  		} else if (float_lit.contains(".")&(float_lit.endsWith("f")|float_lit.endsWith("L")| float_lit.endsWith("l")|float_lit.endsWith("F"))){
+					  		  
+					  			System.out.println("Valid float point Literal: "  +  float_lit);
+					  		} else {
+					  			
+					  		}
+					  		
+					  		
+					  		
+				  	// Invalid such as ASDASDJA and such	
+				  	} else {
+			  			System.out.println("InValid float point Literal: "  + float_lit); 
+			  		
+			  		}
+				  
+			  
+
+/**************************************************************************************************************/
 		  /** B) Integer Literals choice **/
 		  } else if (select.equalsIgnoreCase("B")) {
 			  		
 			  		System.out.println("Enter an integer literal: ");
 			  		String int_lit = console.nextLine(); 		
 			    
-			  		Pattern pattern = Pattern.compile (".*[0-9].*");
+			  		Pattern pattern = Pattern.compile("-?[0-9]*"); // the Rule 
 			  		
+			  		Matcher check = pattern.matcher(int_lit); // What is being checked against the rule
+			  		boolean result = check.matches(); // if the rule applies to it , then it returns true meaning valid input
 			  	
-
-			  		boolean output = !pattern.matcher(int_lit).matches() ; 
-
-			  		if (output = false) {
-			  			System.out.println("Invalid Int Literal :" + int_lit);
+			  		if (result == true) {
+			  			System.out.println("Valid Int Literal: "  + int_lit);
+			  		
 			  		} else {
-			  			System.out.println("Valid Int Literal :"  + int_lit);
+			  			System.out.println("InValid Int Literal: "  + int_lit); 
 			  		}
-			  		
-			  		
-			  		//Pattern pattern = Pattern.compile("[0-9]*");
-			  		
-			  		
-			  		//try {
-				
-					  /// ^-?\\d*(\\.\\d+)?$
-			  		
-			  	/**	boolean isNumber = true; 
-			  		
-			  		isNumber = int_lit.matches("");
-			  		//-?\\d*(\\.\\d+)?$
-			  		//-?\\\\d+(\\\\.\\\\d+)?
-			  		if (isNumber) {
-			  			System.out.println("Valid Int Literal! :"+ int_lit);
-			  			
-			  		} else {
-			  			 System.out.println("Invalid Int Literal! :"+ int_lit);
-			  		} 
-			  		
-				/**	  if (Pattern.matches("[a-Z]+", int_lit ) == false) {
-						  System.out.println("Valid Int Literal! :"+ int_lit);
-					  } else {
-						  System.out.println("Invalid Int Literal! :"+ int_lit);
-					  }
-			  //		} catch (java.util.regex.PatternSyntaxException error) {
-			  			
-			  			 System.out.println("After Catch");
-			  //		} **/ 
-					  
-					  
-					  
-					  
-					  /** Checks whether the input is a valid int literal or not**/
-					 
-					/*  try {		 
-						  int change = Integer.parseInt(int_lit);
-						  System.out.println("Valid Int Literal! "+ int_lit );
-						  
-					  } catch (NumberFormatException error) {
-						  System.out.println("Invalid Int Literal! " + int_lit);
-					  } */ 
-					  /****/
-					  
-					  
-			
-			  
+		
+/**************************************************************************************************************/			  
 		  /** C) String Literals choice **/
 		  } else if(select.equalsIgnoreCase("C")) {
 		
+		
+			  /** Checks whether the input is a valid string literal or not**/
+			  System.out.println("Please place [\"] in the beginning and [\"] at the end. ");
+			  System.out.println("Enter a string literal: ");
+			  String string_lit = console.nextLine(); 
 			  
 			  
+			  String first_Character = Character.toString(string_lit.charAt(0));
+			  int length = string_lit.length()-1; 
+			  String last_Character = Character.toString(string_lit.charAt(length));
+			  
+			  if (first_Character.equals("\"") && (last_Character.equals("\""))) {
+				  System.out.println ("Valid String Literal. Your input of: [" +string_lit+"] .");
+				  
+			  } else {
+				  System.out.println("Invalid String Literal. Please place [\"] in the beginning and [\"] at the end. " );
+			  }
+			
+			 
+		
+			  
+/**************************************************************************************************************/ 
 		  /** D) Variable Names  choice **/
 		  } else if (select.equalsIgnoreCase("D")) { 
 			  
-
+		  System.out.println("Enter a variable name: ");
+		  String var_name = console.nextLine(); 
 			  
 			  
-	      /** Error Selection Output Message  **/
-		  } else {
-			  System.out.println("Error. You did not exactly select A,B,C,D,a,b,c, or d.");   
-		  }
 		  
-		  
-		  
-		  
-		  /** Floating points in Java can come with an f. 
-		   * EXAMPLE: float a = 2.356f , no more than 6 or 7 precision
-		   * points. They can also have 
-		   * 
-		   *  12.12345 is a float.
-		   *  12.123456789 is a double. 15-16 points of precision
-		   * 
-		   *  variable names are just strings followed by an = sign?
-		   *  
-		   *  **/
-		 
-		  
+	  // Below shows a list of invalids in terms to a variable name AND OTHERS
+	  String [] array_keywords = {
+		// Primitive Types and Void
+	    "boolean", "byte", "char", "short", 
+		"int","long","float","double","void",
 		
-		  
-		  float cat = 100; 
-		  float cats2 = 100f; 
-		  float cats3 = 100F; 
-		  float cats4 = 100l; 
-		  float cats5 = 100L;
-
-		  float something= 99.9f;
-		  float something2= 99.99999999999999999999999f;
-	//invalid	  float something3= 99.9;
-		  
-		  System.out.println("Enter a floating point: ");
-		  String float_lit = console.nextLine();
-		  
-		  // if the last letter is not an f or F, that's invalid
-		  // The last letters has to be an f,l,L
-		  
-
-		  String lastCharacter = "";
-		  lastCharacter = float_lit.substring(float_lit.length()-1); 
-		  
-		  if (lastCharacter.equals("f")|lastCharacter.equals("F")|lastCharacter.equals("l")|lastCharacter.equals("L")) {
-			  System.out.println("The last character is valid and is either f, F, l , or L "); 
-		  }
-		  
-		  
-		  
-		  try {
-			 float change = Float.parseFloat(float_lit);
-			  System.out.println("Valid Float Literal! " + float_lit);  
-		  } catch (NumberFormatException error){
-			  System.out.println("Invalid Float Literal! " + float_lit);
-		  }
-		  
-		 
-		   
-		  
-		  
-		  /** Checks whether the input is a valid string literal or not**/
-		  System.out.println("Enter a  string literal: ");
-		  String string_lit = console.nextLine(); 
-				  
-		  // Below shows a list of invalids in terms to a string literal/variable name
-		  String [] array_keywords = {
-			// Primitive Types and Void
-		    "boolean", "byte", "char", "short", 
-			"int","long","float","double","void",
-			
-			// Modifiers
-			"public","protected","private","abstract","static","final", "transient",
-			"volatile",	"synchronized", "native", 
+		// Modifiers
+		"public","protected","private","abstract","static","final", "transient",
+		"volatile",	"synchronized", "native", 
+	
+		//Declarations
+		"class", "interface", "enum","extends", "implements","package","throws",
+	
+		// Control Flow
+		"if","else","then", "try", "catch", "finally", "do", "while", "for",
+		"continue","break", "switch", "case", "default", "throw", "return",
 		
-			//Declarations
-			"class", "interface", "enum","extends", "implements","package","throws",
+		//Misc
+		"this", "new","super","import", "instanceof", "null", "true", "false",
+		"strictfp", "assert", "_", "goto", "const",
 		
-			// Control Flow
-			"if","else","then", "try", "catch", "finally", "do", "while", "for",
-			"continue","break", "switch", "case", "default", "throw", "return",
-			
-			//Misc
-			"this", "new","super","import", "instanceof", "null", "true", "false",
-			"strictfp", "assert", "_", "goto", "const" 
-		 
-		 } ; 
-		  
-		  
-		  
-		 
-		 String check ="false"; 
-		 String answer="";  // to hold the Final Answer of valid or Not
-		 
-		 /* This checks whether the string literal put in is valid or not
-		  * if the string literal equals a keyword, the check becomes True indicating
-		  * that the question "string literal equals a array keyword?" is true thus,
-		  * it is invalid */
-		 
-		 for (int i = 0; i<= array_keywords.length-1 ; i++) {
-			 if (string_lit.equals(array_keywords[i])){
-				 //System.out.println("Invalid String Literal :" + string_lit);
-				 check ="true";
-				 break;
-			 } else {
-				 
-			 }
+		// others
+		"@", "+", "!"
+		
+	 
+	 } ; 
+	    
+	
+	 
+	 String check ="false"; 
+	 String answer="";  // to hold the Final Answer of valid or Not
+	 
+	 /* This checks whether the string literal inputed is valid or not.
+	  * If the string literal equals a keyword, the check becomes True indicating
+	  * that the question "Is the string literal an array keyword?" is YES. (or in this case "True).
+	  * If TRUE/YES, then that means the variable name is invalid */
+	 
+	 for (int i = 0; i<= array_keywords.length-1 ; i++) {
+		 if (var_name.equals(array_keywords[i])){
+			 //System.out.println("Invalid String Literal :" + string_lit);
+			 check ="true";
+			 break;
+		 } else {
 			 
 		 }
 		 
-		 // If true, input is a keyword hence Invalid. Otherwise, it's Valid.
-		 if (check.equals("true")) {
-			 answer="invalid"; 
-		 } else {
-			 answer="valid";
-		 }
-		 
-		 System.out.println("Your input : [" + string_lit +"] is " +answer);
-	      
-	    }
-	  
-	  
-	  
-	  /*	  
-	   // It can't be java keywords & it needs to be letters only & no spaces
-	  System.out.println("Enter a variable name: ");
-	  String var_name = console.nextLine(); 
+	 }
 	 
+	 // If true, input is a keyword hence Invalid. Otherwise, it's Valid.
+	 if (check.equals("true")) {
+		 answer="invalid"; 
+	 } else {
+		 answer="valid";
+	 }
+	 
+	 System.out.println("Your input : [" + var_name +"] is " +answer +" variable name"); 
+			  
+	
+	
+			  
+
+/**************************************************************************************************************/
+	      /** Error Selection Output Message  **/
+	 } else {
+			  System.out.println("Error. You did not exactly select A,B,C,D,a,b,c, or d.");   
+	 }
+		  
+} // End of begin FOR LOOP 		  
+		  
+		  
+		  
+	      
+	    }// End of Public Static Main Void ARgs
 	  
-	   */
 	  
 	  
 	  
-	  
-}
+}// end of public class q1 
